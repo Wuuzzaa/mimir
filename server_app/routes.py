df2f3a67ab2864a5e63f79f7fc0a9a7c287ad41b
Wuuzzaa <jonas-licht@gmx.de>
2024-06-22 15:49:34 +0200

    add yield calculations first commit not running

--- server_app/routes.py
+++ server_app/routes.py
@@ -1,11 +1,12 @@
 from werkzeug.exceptions import MethodNotAllowed
 
 from server_app import app
-from flask import render_template, Blueprint, request, render_template_string
+from flask import render_template, Blueprint, request, render_template_string, jsonify
 
 from .option_calculation import get_iron_condor_data
 from .querys import *
 from .util import get_date_third_friday_of_next_month
+from .yield_calculation import *
 
 
 @app.route('/')
@@ -37,6 +38,7 @@ def results():
 
     elif spread_type == "iron_condor":
         h1_text = "Iron Condor Data"
+
         # get call and put spreads
         call_data = get_put_spread_options(delta=delta, expiration_date=expiration_date, option_type="C", spread_width=spread_width)
         put_data = get_put_spread_options(delta=delta, expiration_date=expiration_date, option_type="P",  spread_width=spread_width)
@@ -47,3 +49,27 @@ def results():
     else:
         raise ValueError(f"Unknown spread type: {spread_type}")
     return render_template('spread_results.html', table_data=table_data, h1_text=h1_text)
+
+@app.route('/yield')
+def _yield():
+    return render_template('yield.html')
+
+@app.route('/calc_yield', methods=['POST'])
+def calculate():
+    data = request.get_json()
+    init_option_premium = float(data['init_option_premium'])
+    max_loss_via_stoploss = float(data['max_loss_via_stoploss'])
+    delta_short_option = float(data['delta_short_option'])
+    bpr = float(data['bpr'])
+    profit = float(data['profit'])
+    days_trade_open = int(data['days_trade_open'])
+    dte = int(data['dte'])
+    fees = float(data['fees'])
+
+    annualized_yield_now = calculate_annualized_yield(profit, days_trade_open, bpr, fees * 2) # *2 cause we need closing orders
+    annualized_yield_expiration = calculate_annualized_yield_at_expiration(dte, bpr, init_option_premium, max_loss_via_stoploss, delta_short_option, fees)
+
+    return jsonify({
+        'annualized_yield_now': annualized_yield_now,
+        'annualized_yield_expiration': annualized_yield_expiration
+    })
---
+++ server_app/yield_calculation.py
@@ -0,0 +1,33 @@
+def calculate_yield(profit: float, bpr: float) -> float:
+    return profit / bpr
+
+def calculate_annualized_yield(profit: float, days_trade_open: int, bpr: float, fees: float) -> float:
+    _yield = calculate_yield(profit-fees, bpr)
+    annualized_yield = _yield * (365 / days_trade_open)
+    annualized_yield = round(annualized_yield, 2)
+    return annualized_yield
+
+def calculate_annualized_yield_at_expiration(dte: int,bpr: float, init_option_premium: float, max_loss_via_stoploss: float, delta_short_option: float, fees: float) -> float:
+    expected_loss = delta_short_option * max_loss_via_stoploss
+    expected_gain = init_option_premium - expected_loss
+    expected_gain -= fees
+    annualized_yield = calculate_annualized_yield(expected_gain, dte, bpr, fees)
+    annualized_yield = round(annualized_yield, 2)
+    return annualized_yield
+
+if __name__ == '__main__':
+    init_option_premium = 80
+    max_loss_via_stoploss = 2 * init_option_premium
+    delta_short_option = 0.2
+    bpr = 410
+    profit = 35
+    days_trade_open = 15
+    dte = 40
+    fees = 7 # opening fees 2 order long and short option for a spread
+
+
+    annualized_yield_now = calculate_annualized_yield(profit, days_trade_open, bpr, fees*2) # *2 cause we need closing orders
+    print(f"{annualized_yield_now} close now")
+
+    annualized_yield_expiration = calculate_annualized_yield_at_expiration(dte, bpr, init_option_premium, max_loss_via_stoploss, delta_short_option, fees)
+    print(f"{annualized_yield_expiration} close at expiration")
